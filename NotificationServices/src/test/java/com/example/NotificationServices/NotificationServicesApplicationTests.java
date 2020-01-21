3bd43089aa8fb51c6515a1770a67d2c05aa2f1a2
piero <pierosteffano@gmail.com>
2020-01-21 17:36:36 -0300

    initial push

---
+++ NotificationServices/src/test/java/com/example/NotificationServices/NotificationServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NotificationServices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
