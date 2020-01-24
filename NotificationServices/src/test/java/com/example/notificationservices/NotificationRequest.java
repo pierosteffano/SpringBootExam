6671c2323670610bf197fe810676137da0f6d788
piero <pierosteffano@gmail.com>
2020-01-24 11:41:30 -0300

    added missing pojo

---
+++ NotificationServices/src/test/java/com/example/notificationservices/NotificationRequest.java
@@ -0,0 +1,24 @@
+package com.example.notificationservices;
+
+public class NotificationRequest {
+    private final String orderId;
+    private final String notificationEmail;
+
+    public NotificationRequest(String orderId, String notificationEmail) {
+        this.orderId=orderId;
+        this.notificationEmail=notificationEmail;
+    }
+
+    public String getOrderId() {
+        return orderId;
+    }
+
+    public String getNotificationEmail() {
+        return notificationEmail;
+    }
+
+    @Override
+    public String toString() {
+        return "{orderId: ".concat(this.orderId).concat(",:notificationEmail").concat(this.notificationEmail);
+    }
+}
